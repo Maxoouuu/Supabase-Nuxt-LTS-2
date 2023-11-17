b3231571583fe522e033f9381a6ad54ac10e9e0c
Maxime Coriton <maxime.coriton@gmail.com>
2023-11-17 15:43:38 +0100

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/supabase',
+    '@nuxtjs/robots',
+    'nuxt-simple-sitemap',
+    '@pinia/nuxt',
+  ],
+  css: ['@/assets/css/main.css'],
+
+})
---
+++ pages/login.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+
+const loading = ref(false)
+const email = ref('')
+
+const handleLogin = async () => {
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signInWithOtp({
+      email: email.value,
+      options: {
+        emailRedirectTo: 'http://localhost:3000/confirm',
+      }
+    })
+    if (error) throw error
+    alert('Check your email for the login link!')
+  } catch (error) {
+    console.error(error)
+    alert(error.error_description || error.message)
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleLogin" class="row flex-center flex">
+    <div class="col-6 form-widget">
+      <h1 class="header">Supabase + Nuxt 3</h1>
+      <p class="description">Sign in via magic link with your email below</p>
+      <div>
+        <input class="inputField" type="email" placeholder="Your email" v-model="email" />
+      </div>
+      <div>
+        <input
+          type="submit"
+          class="button block"
+          :value="loading ? 'Loading' : 'Send magic link'"
+          :disabled="loading"
+        />
+      </div>
+    </div>
+  </form>
+</template>
